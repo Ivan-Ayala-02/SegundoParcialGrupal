758ab3e9e1c8989fb3c65554c78e5a6102886f4c
Ivan-Ayala-02 <ivanayala1324@gmail.com>
2025-07-12 16:42:53 -0300

    Borrando archivo inecesario

--- Consola/recuperatorio.py
+++
@@ -1,9 +0,0 @@
-# --------------------------------------------------------------------------------------
-#   EJECUCION FUNCIONES
-# --------------------------------------------------------------------------------------
-
-from modulos.mini_juegos.mini_juegos import minijuego_buscaminas
-from modulos.configuracion import diccionario_datos_tablero
-
-minijuego_buscaminas(5,5,1, diccionario_datos_tablero)
-
